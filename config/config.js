203bd9451ff11f3a4797953dd16959a84450fc7c
muammer alkis <muammeralkis@gmail.com>
2020-07-22 21:53:39 +0300

    first commit

--- config/config.js
+++ config/config.js
@@ -1,13 +1,7 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 let config = {
-    apiKey: "AIzaSyDZF6afKDFTDd68sBzlekOAbgNIR3xrMDI",
-    authDomain: "salerent-2366e.firebaseapp.com",
-    databaseURL: "https://salerent-2366e.firebaseio.com",
-    projectId: "salerent-2366e",
-    storageBucket: "salerent-2366e.appspot.com",
-    messagingSenderId: "427757428441",
-    appId: "1:427757428441:web:91177e68ca3add3364e8c8"
+    // Here is your specific Firebase config information
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ experimentalForceLongPolling: true });
